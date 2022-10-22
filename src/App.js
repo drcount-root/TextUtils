74b207ba804a95d5931980a03a6fff5ca0885c8a
Subham Sahu <about.subhamsahu@gmail.com>
2022-10-22 21:29:07 +0530

    22nd Octo 2022

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar title={TextUtils} />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze" />
+        {/* <About/> */}
       </div>
     </>
   );
